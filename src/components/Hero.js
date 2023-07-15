9651261ee060ddacc65f0300ad91a420e8a91609
Jugal Mehra <jugal.mehra@indianic.com>
2023-07-15 17:05:38 +0530

    hero

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./css/hero.css"
+function Hero() {
+  return (
+    <div class="hero">
+    <div class="hero-msg">
+        <p>You are on amazon.com. You can also shop on Amazon India for millions of products with fast local delivery. <a href="https://www.amazon.in">Click here to go to amazon.in</a></p>
+    </div>
+    </div>
+  )
+}
+
+export default Hero
